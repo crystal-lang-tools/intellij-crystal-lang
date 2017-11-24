fc20f2e1909f38965fe42f4ac44f14e2615a9f57
Kingsley <kingsleyhendrickse@me.com>
2017-11-24 11:23:10 +0000

    bnf approach 1

--- src/main/java/net/kenro/ji/jin/crystal/lexer/CrystalHighlightLexer.java
+++ src/main/java/net/kenro/ji/jin/crystal/lexer/CrystalHighlightLexer.java
@@ -1,16 +1,10 @@
 package net.kenro.ji.jin.crystal.lexer;
 
 import com.intellij.lexer.FlexAdapter;
-import com.intellij.lexer.LookAheadLexer;
-import com.intellij.lexer.MergingLexerAdapter;
-import com.intellij.psi.tree.TokenSet;
-import net.kenro.ji.jin.crystal.psi.CrystalTokens;
 
-import java.io.Reader;
-
-public final class CrystalHighlightLexer extends LookAheadLexer {
+public class CrystalHighlightLexer extends FlexAdapter {
     public CrystalHighlightLexer() {
-        super(new MergingLexerAdapter(new FlexAdapter(new net.kenro.ji.jin.crystal.lexer._CrystalLexer((Reader) null)), TokenSet.create(CrystalTokens.WS, CrystalTokens.STRING)), 10);
+        super(new net.kenro.ji.jin.crystal.lexer._CrystalLexer(null));
     }
 }
 
--- src/main/java/net/kenro/ji/jin/crystal/psi/cst/Module.java
+++
@@ -1,9 +0,0 @@
-package net.kenro.ji.jin.crystal.psi.cst;
-
-import net.kenro.ji.jin.crystal.psi.CrystalElements;
-
-public class Module extends CrystalElement {
-    protected Module() {
-        super(CrystalElements.Module);
-    }
-}

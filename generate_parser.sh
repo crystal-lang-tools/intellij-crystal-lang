2090c03aeecde1da9b09156d16e77dc4fccc118e
Kingsley <kingsleyhendrickse@me.com>
2017-11-25 07:18:08 +0000

    clean up sh files

--- generate_parser.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-GEN_DIR=src/main/gen
-JFLEX="jflex-1.7.0-SNAPSHOT.jar"
-FLEX_FILE=src/main/java/net/kenro/ji/jin/crystal/lexer/Crystal.flex
-SKEL_FILE=idea-flex.skeleton
-java -cp "$JFLEX" -jar $JFLEX -d $GEN_DIR --skel $SKEL_FILE $FLEX_FILE
